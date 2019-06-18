06872a15fedea618ba13f72482f1926209813af7
Dani Lyn <dlyn1110@gmail.com>
2019-06-18 15:50:40 +0000

    Automatically backed up by Learn

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -7,35 +7,13 @@ export const addQuote = quote => {
   };
 };
 
-export const removeQuote = quote => {
+export const removeQuote = quoteId => {
   return {
     type: 'REMOVE_QUOTE',
     quote: quoteID
   };
 };
 
-export const upvoteQuote = quote => {
-  return {
-    type: 'UPVOTE-QUOTE',
-    quoteID
-  };
-};
-
-export const upvoteQuote = quote => {
-  return {
-    type: 'UPVOTE_QUOTE',
-    quoteID
-  };
-};
-
-export const upvoteQuote = quote => {
-  return {
-    type: 'UPVOTE_QUOTE',
-    quoteID
-  };
-};
-
-
 export const upvoteQuote = quoteId => {
   return {
     type: 'UPVOTE_QUOTE',
@@ -43,13 +21,6 @@ export const upvoteQuote = quoteId => {
   };
 };
 
-
-export const upvoteQuote = quoteId => {
-  return {
-    type: 'UPVOTE_QUOTE',
-    quoteID
-  };
-};
 
 export const downvoteQuote = quoteId => {
   return {
