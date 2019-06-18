d87f84fce4cc072c0874804b3a287e78960608d8
Dani Lyn <dlyn1110@gmail.com>
2019-06-18 15:47:52 +0000

    Automatically backed up by Learn

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -4,12 +4,56 @@ export const addQuote = quote => {
   return {
     type: 'ADD_Quote',
     quote: {...quote}
-  }
-}
+  };
+};
 
 export const removeQuote = quote => {
   return {
     type: 'REMOVE_QUOTE',
     quote: quoteID
-  }
-}
+  };
+};
+
+export const upvoteQuote = quote => {
+  return {
+    type: 'UPVOTE-QUOTE',
+    quoteID
+  };
+};
+
+export const upvoteQuote = quote => {
+  return {
+    type: 'UPVOTE-QUOTE',
+    quoteID
+  };
+};
+
+export const upvoteQuote = quote => {
+  return {
+    type: 'UPVOTE-QUOTE',
+    quoteID
+  };
+};
+
+
+export const upvoteQuote = quoteId => {
+  return {
+    type: 'UPVOTE-QUOTE',
+    quoteID
+  };
+};
+
+
+export const upvoteQuote = quoteId => {
+  return {
+    type: 'UPVOTE-QUOTE',
+    quoteID
+  };
+};
+
+export const downvoteQuote = quoteId => {
+  return {
+    type: 'UPVOTE_QUOTE',
+    quoteId
+  };
+};
