06d156a3881f39089fa659c26f19918befcc8c91
Dani Lyn <dlyn1110@gmail.com>
2019-06-18 15:43:41 +0000

    Automatically backed up by Learn

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,15 @@
 // TODO: Create action creators as defined in tests
+
+export const addQuote = quote => {
+  return {
+    type: 'ADD_Quote',
+    quote: {...quote}
+  }
+}
+
+export const removeQuote = quote => {
+  return {
+    type: 'REMOVE_QUOTE',
+    quote: quoteID
+  }
+}
