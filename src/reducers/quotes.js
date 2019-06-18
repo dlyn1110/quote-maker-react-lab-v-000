aef62f087f86a7c3e18a284c3245f7112aa73564
Dani Lyn <dlyn1110@gmail.com>
2019-06-18 16:25:54 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,9 +1,43 @@
-import uuid from 'uuid';
+import uuid from "uuid";
 
 export default (state = [], action) => {
 
+  let index;
+  let quote;
 
-  default:
-  return state;
- }
+  switch (action.type) {
+
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+        ...state.slice(index + 1)
+      ];
+
+    case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+
+      if (quote.votes > 0) {
+        return [
+          ...state.slice(0, index),
+          {...quote, votes: quote.votes - 1},
+          ...state.slice(index + 1)
+        ];
+      } else {
+        return state;
+      }
+
+    default:
+      return state;
+    }
 }
